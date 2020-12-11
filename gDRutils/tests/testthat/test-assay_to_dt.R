8741e2ad7fef70639ca6295a800c14b8d1849fe3
czechb3 <bartosz.czech@contractors.roche.com>
2020-12-11 15:47:46 +0100

    Fix unit test

--- gDRutils/tests/testthat/test-assay_to_dt.R
+++ gDRutils/tests/testthat/test-assay_to_dt.R
@@ -5,9 +5,11 @@ context("assay_to_dt")
 test_that("assay_to_dt works as expected", {
   SE <- readRDS(system.file(package ="gDRutils", "inst", "testdata", "exemplarySE.rds"))
   normalized <- assay_to_dt(SE, "Normalized")
-  normalized <- normalized[, order(colnames(normalized))]
+  data.table::setcolorder(normalized, sort(names(normalized)))
+  data.table::setorder(normalized)
   normalizedRef <- data.table::as.data.table(readRDS(system.file(package ="gDRutils", "inst", "testdata", "normalizedExemplary.rds")))
-  normalizedRef <- normalizedRef[, order(colnames(normalizedRef))]
-  testthat::expect_identical(sort(normalized), sort(normalizedRef))
+  data.table::setcolorder(normalizedRef, sort(names(normalizedRef)))
+  data.table::setorder(normalizedRef)
+  testthat::expect_equal(normalized, normalizedRef)
 })
 
