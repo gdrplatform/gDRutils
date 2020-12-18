b75c9375902c429b67767e6757fbee9adcddf154
vuonga1 <vuong.allison@gene.com>
2020-12-17 18:16:19 -0800

    add documentation

--- gDRutils/R/get_header.R
+++ gDRutils/R/get_header.R
@@ -1,5 +1,17 @@
 #' @include global_cache.R
 
+#' Get the headers for a header field
+#'
+#' Get the expected headers for a specific data type
+#' 
+#' @param x string of field (data type) to return headers for
+#' 
+#' @return character vector of headers for field \code{x}
+#' 
+#' @details
+#' If \code{get_header} is called with no values, the entire available header list is returned.
+#' @examples
+#' get_header("manifest")
 #' @export
 get_header <- function(x = NULL) {
   global_cache$get_head(x)
