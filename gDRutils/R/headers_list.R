87ff5dd74b3f9a7aa4dc9462a848afc575626b79
vuonga1 <vuong.allison@gene.com>
2021-01-20 08:01:29 -0800

    minor style improvement to headers_list

--- gDRutils/R/headers_list.R
+++ gDRutils/R/headers_list.R
@@ -125,7 +125,7 @@
     get_identifier("drugname"),
     "Concentration",
     paste0(c(paste0(get_identifier("drugname"), "_"), "Concentration_"), 
-      sort(c(2:10, 2:10)))
+      rep(2:10, each = 2))
   )
 
   HEADERS_LIST[["ordered_2"]] <- c(
