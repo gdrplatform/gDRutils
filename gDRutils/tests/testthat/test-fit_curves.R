e8f7c7ab6a751785c198daca4cc23d27ede9b725
vuonga1 <vuong.allison@gene.com>
2021-03-29 17:42:55 -0700

    add numeric type assertions and tests for logistic_4parameters

--- gDRutils/tests/testthat/test-fit_curves.R
+++ gDRutils/tests/testthat/test-fit_curves.R
@@ -161,3 +161,36 @@ test_that(".estimate_xc50 works as expected", {
   expect_equal(gDRutils:::.estimate_xc50(c(0.1, 0.2, 0.3, 0.4)), -Inf)
   expect_equal(gDRutils:::.estimate_xc50(c(0.1, 0.2, 0.6, 0.7)), NA)
 })
+
+
+test_that("logistic_4parameters works as expected", {
+  c <- 1
+  Vinf <- 0.1
+  V0 <- 1 
+  h <- 2  
+  EC50 <- 0.5
+
+  # Non-numeric values cause an error. 
+  expect_error(logistic_4parameters(c = "non-numeric_entry", 
+				    Vinf = Vinf,
+				    V0 = V0,
+				    EC50 = EC50,
+				    h = h))
+
+  # Normal fit.
+  v <- logistic_4parameters(c = c,
+                            Vinf = Vinf,
+                            V0 = V0,
+                            EC50 = EC50,
+                            h = h) 
+  expect_equal(v, 0.28)
+
+  # Flat fit.
+  EC50 <- 0
+  v <- logistic_4parameters(c = c,
+                            Vinf = Vinf,
+                            V0 = V0,
+                            EC50 = EC50,
+                            h = h) 
+  expect_equal(v, Vinf)  
+})
