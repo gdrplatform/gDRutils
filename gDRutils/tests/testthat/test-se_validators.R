59d190b7cabc01087021e71283368ea04f7d18ce
czechb3 <bartosz.czech@contractors.roche.com>
2021-04-23 12:40:53 +0200

    Add unit tests for SE validator

--- gDRutils/tests/testthat/test-se_validators.R
+++ gDRutils/tests/testthat/test-se_validators.R
@@ -3,3 +3,32 @@ test_that("is_valid_se_assay_name works as expected", {
   expect_error(is_valid_se_assay_name(se, "apple"))
   expect_true(is.null(is_valid_se_assay_name(se, "orange")))
 })
+
+test_that("validate_SE works as expected", {
+  se1 <- SummarizedExperiment::SummarizedExperiment(assays = list("orange" = matrix(1, 1, 1)))
+  expect_error(validate_SE(se1))
+  x <- NumericList(split(runif(1000), factor(sample(50, 1000, replace=TRUE), 1:50))) 
+  se2 <- SummarizedExperiment::SummarizedExperiment(assays = 
+                                                      list("RawTreated" = BumpyMatrix::BumpyMatrix(x, c(10, 5)),
+                                                           "Controls" = BumpyMatrix::BumpyMatrix(x, c(10, 5)),
+                                                           "Normalized" = BumpyMatrix::BumpyMatrix(x, c(10, 5)),
+                                                           "RefGRvalue" = BumpyMatrix::BumpyMatrix(x, c(10, 5)),
+                                                           "RefRelativeViability" = BumpyMatrix::BumpyMatrix(x, c(10, 5)),
+                                                           "DivisionTime" = BumpyMatrix::BumpyMatrix(x, c(10, 5)),
+                                                           "Averaged" = BumpyMatrix::BumpyMatrix(x, c(10, 5)),
+                                                           "Metrics" = BumpyMatrix::BumpyMatrix(x, c(10, 5))))
+  
+  expect_error(validate_SE(se2))
+  se2@metadata <- list("experiment_metadata",
+                       "df_",
+                       "Keys","df_raw_data",
+                       "fit_parameters",
+                       "drug_combinations")
+  names(se2@metadata) <- c("experiment_metadata",
+                           "df_",
+                           "Keys","df_raw_data",
+                           "fit_parameters",
+                           "drug_combinations")
+  expect_error(validate_SE(se2))
+  # TODO: add real SE example
+})
