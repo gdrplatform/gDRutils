7f95a8be219fa0831559fa0fe9b20057cbbaf058
czechb3 <bartosz.czech@contractors.roche.com>
2021-05-19 14:15:27 +0200

    Update as per review

--- gDRutils/R/merge_SE.R
+++ gDRutils/R/merge_SE.R
@@ -34,28 +34,16 @@ merge_SE <- function(SElist,
   }
   
   SEdata$treatment_md$cId <- NULL
-  metadataNames <- unique(unlist(lapply(SElist, function(x) {
-    names(S4Vectors::metadata(x))
-  })))
-  metadataSE <- vector("list", length(metadataNames))
-  names(metadataSE) <- metadataNames
+  metadataNames <- identify_unique_se_metadata_fields(SElist)
+  metadataSE <- merge_metadata(SElist, metadataNames)
   
-  metadataSEAll <- lapply(names(metadataSE), function(x) {
-    mergedMetadata <- NULL
-    do.call(c, lapply(names(SElist), function(SE) {
-      metaObj <- list(S4Vectors::metadata(SElist[[SE]])[[x]])
-      names(metaObj) <- SE
-      metaObj
-    }))
-  })
-  names(metadataSEAll) <- metadataNames
-  initialSE <- SummarizedExperiment::SummarizedExperiment(assays = list(Normalized = normalized$BM,
+  se <- SummarizedExperiment::SummarizedExperiment(assays = list(Normalized = normalized$BM,
                                                                         Averaged = averaged$BM,
                                                                         Metrics = metrics$BM),
                                                           colData = SEdata$condition_md,
                                                           rowData = SEdata$treatment_md,
                                                           metadata = metadataSE)
-  initialSE
+  se
 }
 
 
@@ -73,14 +61,14 @@ merge_SE <- function(SElist,
 #' @return BumpyMatrix or list with data.table + BumpyMatrix
 #' @export
 #'
-#' @examples
 merge_assay <- function(SElist,
                         assay_name,
-                        discard_keys = NULL,
-                        additional_col_name = "data_source") {
+                        additional_col_name = "data_source",
+                        discard_keys = NULL) {
   
   checkmate::assert_list(SElist, types = "SummarizedExperiment")
   checkmate::assert_string(assay_name)
+  lapply(SElist, function(x) validate_se_assay_name(x, assay_name))
   checkmate::assert_true(all(unlist(lapply(SElist, function(x) {
     assay_name %in% names(SummarizedExperiment::assays(x))
   }))))
@@ -94,8 +82,48 @@ merge_assay <- function(SElist,
     data.table::rbindlist(lapply(names(SElist), function(x)
       convert_se_assay_to_dt(SElist[[x]], assay_name)[, eval(additional_col_name) := rep_len(x, .N)]), fill = TRUE)
   }
-  DT$rId <- NULL
-  DT$cId <- NULL
+  DT$rId <- DT$cId <- NULL
   BM <- df_to_bm_assay(DT, discard_keys = c(discard_keys, additional_col_name))
   list(DT = DT, BM = BM)
+}
+
+
+#' Indetify unique se metadata fields from the list of SEs
+#'
+#' @param SElist named list of Summarized Experiments
+#'
+#' @return vector of unique names of metadata
+#' @export
+#'
+identify_unique_se_metadata_fields <- function(SElist) {
+  checkmate::assert_list(SElist, types = "SummarizedExperiment")
+  
+  unique(unlist(lapply(SElist, function(x) {
+    names(S4Vectors::metadata(x))
+  })))
+}
+
+#' Merge metadata
+#'
+#' @param SElist named list of Summarized Experiments
+#' @param metadata_fields vector of metadata names that will be merged
+#'
+#' @return list of merged metadata
+#' @export
+#'
+merge_metadata <- function(SElist,
+                           metadata_fields) {
+  
+  checkmate::assert_list(SElist, types = "SummarizedExperiment")
+  checkmate::assert_character(metadata_fields)
+  
+  metadataSEAll <- lapply(metadata_fields, function(x) {
+    do.call(c, lapply(names(SElist), function(SE) {
+      metaObj <- list(S4Vectors::metadata(SElist[[SE]])[[x]])
+      names(metaObj) <- SE
+      metaObj
+    }))
+  })
+  names(metadataSEAll) <- metadata_fields
+  metadataSEAll
 }
--- gDRutils/tests/testthat/test-merge_SE.R
+++ gDRutils/tests/testthat/test-merge_SE.R
@@ -9,13 +9,31 @@ test_that("merge_assay works as expected", {
   checkmate::expect_class(normalizedMerged[[2]], "BumpyDataFrameMatrix")
 })
 
+test_that("merge_metadata and identify_unique_se_metadata_fields work as expected", {
+  listSE <- lapply(list.files(system.file(package = "gDRtestData", "testdata"),
+                              "final", full.names = TRUE)[1:2], readRDS)
+  names(listSE) <- c("combo1", "combo2")
+  metadata_fields <- identify_unique_se_metadata_fields(listSE)
+  mergedMetadata <- merge_metadata(listSE, metadata_fields)
+  expect_identical(names(mergedMetadata), metadata_fields)
+  expect_identical(names(mergedMetadata$experiment_metadata), names(listSE))
+  
+  listSE2 <- listSE
+  newMetaName <- "dummy_meta"
+  S4Vectors::metadata(listSE2$combo1)[[newMetaName]]<- list()
+  metadata_fields2 <- identify_unique_se_metadata_fields(listSE2)
+  expect_true(newMetaName %in% metadata_fields2)
+  mergedMetadata2 <- merge_metadata(listSE2, metadata_fields2)
+  expect_true(newMetaName %in% names(mergedMetadata2))
+})
+
 test_that("merge_SE works as expected", {
   listSE <- lapply(list.files(system.file(package = "gDRtestData", "testdata"),
                               "final", full.names = TRUE)[1:2], readRDS)
   names(listSE) <- c("combo1", "combo2")
   mergedSE <- merge_SE(listSE)
-  S4Vectors::metadata(mergedSE)
   checkmate::expect_class(mergedSE, "SummarizedExperiment")
+  S4Vectors::metadata(mergedSE)[["df_raw_data"]] <- list(NULL)
   validate_SE(mergedSE)
   additional_col_name <- "QCS"
   mergedSE2 <- merge_SE(listSE, additional_col_name)
