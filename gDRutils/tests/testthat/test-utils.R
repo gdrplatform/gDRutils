4870c4c1887c2c997f4911020f7c309c4a58039d
vuonga1 <vuong.allison@gene.com>
2021-06-29 18:46:37 -0700

    add test for utils helpers

---
+++ gDRutils/tests/testthat/test-utils.R
@@ -0,0 +1,9 @@
+library(testthat); library(gDRutils); source("setup.R")
+
+test_that(".clean_key_inputs works as expected", {
+  keys <- LETTERS[1:5]
+  cols <- LETTERS[1:3]
+  expect_warning({out <- gDRutils:::.clean_key_inputs(keys, cols)})
+  expect_equal(out, cols)
+})
+
