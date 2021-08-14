ec7c170a576e3f15ea123e7e9d04ba7c38eb9fae
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2021-08-14 22:10:55 +0200

    feat: change the logic of 'get_SE_identifiers'
    Details:
     - set 'add_defaults' parameter to TRUE by default
     - extend the logic for NULL/not-NULL id_type

--- gDRutils/R/se_metadata.R
+++ gDRutils/R/se_metadata.R
@@ -96,20 +96,22 @@ get_SE_keys <- function(se, key_type = NULL) {
 #' @rdname identifiers
 #' @export
 #'
-get_SE_identifiers <- function(se, id_type = NULL, add_defaults = FALSE) {
+get_SE_identifiers <- function(se, id_type = NULL, add_defaults = TRUE) {
   ## `strict = FALSE` is present for backwards compatibility.
   ## If the identifier does not exist on the se object, we fetch it from the environment.
-  value <- .get_SE_metadata(se, name = "identifiers", subname = id_type, strict = FALSE)
-  if (is.null(value)) {
-    value <- get_identifier(id_type)
-  } else { 
-    if (add_defaults) {
-      def_value <- get_identifier()
-      c(value, def_value[!names(def_value) %in% names(value)])
+  se_value <- .get_SE_metadata(se, name = "identifiers", subname = id_type, strict = FALSE)
+  
+  value <- if (!add_defaults) {
+    se_value
+  } else {
+    def_value <- get_identifier(id_type)
+    if (is.null(id_type)) {
+      c(se_value, def_value[!names(def_value) %in% names(se_value)])
     } else {
-      value
+      ifelse(is.null(se_value), def_value, se_value)
     }
   }
+  value
 }
 
 
