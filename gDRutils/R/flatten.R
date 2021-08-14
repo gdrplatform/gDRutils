6a90b5a1b877ed3d3fad234cb3723e6a9453c1c3
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2021-08-14 22:07:37 +0200

    feat: make 'flatten' returning data without 'identifiers' attritubute

--- gDRutils/R/flatten.R
+++ gDRutils/R/flatten.R
@@ -70,7 +70,5 @@ flatten <- function(tbl, groups, wide_cols, sep = "_") {
   filtered <- out[lapply(out, nrow) > 0L]
   reduced <- Reduce(function(x, y) merge(x, y, by = intersect(names(x), names(y))), filtered)
   
-  #return 'identifiers' attribute provided by input tbl
-  attributes(reduced) <- c(attributes(reduced), attributes(tbl)["identifiers"])
   reduced
 }
