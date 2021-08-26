2d5e59280c49fd188bb05257ce3603750b4d20b2
Bartosz Czech <bartosz.czech@contractors.roche.com>
2021-08-26 12:49:58 +0200

    test: fix unit tests

--- gDRutils/R/fit_curves.R
+++ gDRutils/R/fit_curves.R
@@ -83,7 +83,7 @@ fit_curves <- function(df_,
   med_concs <- stats::median(concs)
   min_concs <- min(concs)
 
-  concsNA <- all(is.na(concs))
+  concsNA <- is.na(concs)
   concs <- ifelse(concsNA, 0, concs)
   
   if ("RV" %in% normalization_type) {
