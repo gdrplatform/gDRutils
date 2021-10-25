a7c19f8a2d6520cbabd1497d0dd64a49f3820f38
vuonga1 <vuonga1@gene.com>
2021-10-25 08:50:59 -0700

    chore: modify prediction functions to assert arg length

--- gDRutils/R/fit_curves.R
+++ gDRutils/R/fit_curves.R
@@ -328,6 +328,7 @@ predict_efficacy_from_conc <- function(c, x_inf, x_0, ec50, h) {
   checkmate::assert_numeric(x_0)
   checkmate::assert_numeric(ec50)
   checkmate::assert_numeric(h)
+  assert_equal_input_len(outlier = c, x_inf, x_0, ec50, h)
 
   x_inf + (x_0 - x_inf) / (1 + (c / ec50) ^ h)
 }
@@ -338,6 +339,7 @@ predict_efficacy_from_conc <- function(c, x_inf, x_0, ec50, h) {
 #' @seealso predict_efficacy_from_conc .calculate_x50
 #' @export
 predict_conc_from_efficacy <- function(efficacy, x_inf, x_0, ec50, h) {
+  assert_equal_input_len(outlier = efficacy, x_inf, x_0, ec50, h)
   if (efficacy > x_0) {
     0
   } else {
