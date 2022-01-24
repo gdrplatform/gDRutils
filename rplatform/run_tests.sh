2990f6af65636cffc6a8b2e4b541a47d095187d6
vuonga1 <vuonga1@gene.com>
2022-01-24 15:17:41 -0800

    chore: update to latest checkDependencies

--- rplatform/run_tests.sh
+++ rplatform/run_tests.sh
@@ -9,7 +9,7 @@ echo "Working directory contains: `ls | tr '\n' ' '`"
 set -e
 
 echo ">>>>>>>> RUNNING LINTER"
-Rscript -e "gDRstyle::lintPkgInDir('/mnt/vol/gDRutils')"
+Rscript -e "gDRstyle::lintPkgDirs('/mnt/vol/gDRutils')"
 
 echo ">>>>> RUNNING UNIT TESTS"
 Rscript -e "testthat::test_local(path = '/mnt/vol/gDRutils', stop_on_failure = TRUE)"
@@ -21,10 +21,5 @@ Rscript -e "gDRstyle::checkDependencies('/mnt/vol/gDRutils')"
 #echo ">>>>> RUNNING DEVTOOLS::CHECK()"
 #sudo R CMD check --no-build-vignettes --no-manual --no-tests /mnt/vol/gDRwrapper
 
-
 echo ">>>>>>>> RUNNING CHECK DEPENDENCIES"
-Rscript -e "gDRstyle::checkDependencies('/mnt/vol/gDRutils')"
-
-
-echo ">>>>>>>> RUNNING CHECK DEPENDENCIES"
-Rscript -e "gDRstyle::checkDependencies('/mnt/vol/gDRutils')"
+Rscript -e "gDRstyle::checkDependencies(dep_path='/mnt/vol/dependencies.yaml', desc_path='/mnt/vol/gDRutils/DESCRIPTION')"
