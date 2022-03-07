71e9c316d6086cf17d39693d30ff54e65181460b
vuonga1 <vuonga1@gene.com>
2022-03-07 15:19:25 -0800

    chore: move deprecated fxns

--- gDRutils/R/deprecated.R
+++ gDRutils/R/deprecated.R
@@ -3,3 +3,10 @@ logistic_4parameters <- function(c, Vinf, V0, EC50, h) {
   .Deprecated("predict_efficacy_from_conc")
   predict_efficacy_from_conc(c, Vinf, V0, EC50, h)
 }
+  
+
+#' @export
+get_identifier <- function(k = NULL, simplify = TRUE) {
+  .Deprecated("get_env_identifiers")
+  get_env_identifiers(k = k, simplify = simplify)
+}
