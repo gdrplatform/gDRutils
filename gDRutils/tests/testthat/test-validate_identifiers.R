35ca4eeae54d60da60551bf9a39f8f5495104e28
vuonga1 <vuonga1@gene.com>
2022-03-14 15:14:44 -0700

    test: tidy tests for validate_identifiers

--- gDRutils/tests/testthat/test-validate_identifiers.R
+++ gDRutils/tests/testthat/test-validate_identifiers.R
@@ -80,7 +80,7 @@ test_that(".check_polymapped_identifiers works as expected", {
   expect_equal(obs, sprintf("more than one mapping for identifier(s): '%s'\n", exp))
 })
 
-test_that("gDRutils:::.validate_polymapped_identifiers works as expected", {
+test_that("gDRutils:::.modify_polymapped_identifiers works as expected", {
   # Set up.
   nrow <- 5
   exp_one_ids <- get_expect_one_identifiers()
@@ -93,41 +93,57 @@ test_that("gDRutils:::.validate_polymapped_identifiers works as expected", {
   names(df) <- id_map
 
   # All singletons.
-  obs <- gDRutils:::.validate_polymapped_identifiers(df, exp_one_ids, id_map)
+  obs <- gDRutils:::.modify_polymapped_identifiers(df, exp_one_ids, id_map)
   expect_equal(obs, id_map)
 
   # Some polymappings.
   some_poly_map <- id_map
   some_poly_map[[1]] <- c(some_poly_map[[1]], "extra_item") 
-  obs <- gDRutils:::.validate_polymapped_identifiers(df, exp_one_ids, id_map = some_poly_map)
+  obs <- gDRutils:::.modify_polymapped_identifiers(df, exp_one_ids, id_map = some_poly_map)
   expect_equal(obs, id_map)
 
   # All polymappings.
   all_poly_map <- id_map
   all_poly_map <- lapply(seq_along(all_poly_map), function(x) c(all_poly_map[[x]], "extra_item"))
   names(all_poly_map) <- names(id_map)
-  obs <- gDRutils:::.validate_polymapped_identifiers(df, exp_one_ids, id_map = all_poly_map)
+  obs <- gDRutils:::.modify_polymapped_identifiers(df, exp_one_ids, id_map = all_poly_map)
   expect_equal(obs, id_map)
+
+  # Multiple valid polymappings.
+  multi_valid_map <- id_map
+  multi_valid_map[[1]] <- c(multi_valid_map[[1]], multi_valid_map[[2]])
+  expect_error(gDRutils:::.modify_polymapped_identifiers(df, exp_one_ids, id_map = multi_valid_map),
+    regex = sprintf("multiple valid identifier values found for identifier: '%s'", names(multi_valid_map)[1]))
 })
 
 test_that("validate_identifiers works as expected", {
-  # Set up.
   nrow <- 5
+  exp_one_ids <- get_expect_one_identifiers()
   req_ids <- get_required_identifiers()
-  nids <- length(req_ids)
+  nids <- length(exp_one_ids)
 
   ids <- as.list(LETTERS[seq(nids)])
-  names(ids) <- req_ids
+  names(ids) <- exp_one_ids
 
   df <- as.data.frame(matrix(rep(0, nids * nrow), nrow = nrow, ncol = nids))
   names(df) <- ids
 
+  # Single mapping all valid.
+  expect_equal(validate_identifiers(df, identifiers = ids, req_ids = req_ids, exp_one_ids = exp_one_ids), ids)
+
   # Single mapping but does not exist.
   single_map_ids <- ids
   single_map_ids[1] <- "Cinderella"
-  expect_error(validate_identifiers(df, identifiers = single_map_ids, req_ids = req_ids))
+  expect_error(validate_identifiers(df, identifiers = single_map_ids, req_ids = req_ids, exp_one_ids = exp_one_ids))
 
-  # TODO
-  # Polymapping not
-  expect_error(validate_identifiers(df, identifiers = ))
+  # Polymapping one exists.
+  poly_map_ids <- ids
+  poly_map_ids[[1]] <- c(poly_map_ids[[1]], "BOGUS")
+  expect_equal(validate_identifiers(df, identifiers = poly_map_ids, req_ids = req_ids, exp_one_ids = exp_one_ids), ids)
+
+  # Polymapping both exists.
+  multi_valid_ids <- ids
+  multi_valid_ids[[1]] <- c(multi_valid_ids[[1]], multi_valid_ids[[2]])
+  expect_error(validate_identifiers(df, identifiers = multi_valid_ids, req_ids = req_ids, exp_one_ids = exp_one_ids),
+    regex = sprintf("multiple valid identifier values found for identifier: '%s'", names(ids)[1]))
 })
