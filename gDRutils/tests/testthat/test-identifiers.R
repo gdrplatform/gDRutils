b913d48687d8ab45a776e03c5db247d8f49ce3ad
vuonga1 <vuonga1@gene.com>
2022-03-14 16:29:26 -0700

    test: fix get_identifier tests

--- gDRutils/tests/testthat/test-identifiers.R
+++ gDRutils/tests/testthat/test-identifiers.R
@@ -51,12 +51,12 @@ test_that("get_SE_identifier works with untreated_tag", {
 })
 
 test_that("get_expect_one_identifiers works as expected", {
-  expect_true(all(get_expect_one_identifiers() %in% get_env_identifiers))
+  expect_true(all(get_expect_one_identifiers() %in% names(get_env_identifiers())))
   expect_true(length(get_expect_one_identifiers()) > 1L)
 })
 
 test_that("get_required_identifiers works as expected", {
-  expect_true(all(get_required_identifiers() %in% get_env_identifiers))
+  expect_true(all(get_required_identifiers() %in% names(get_env_identifiers())))
   expect_true(length(get_required_identifiers()) > 1L)
   expect_true(all(get_required_identifiers() %in% get_expect_one_identifiers()))
 })
