9908627d51ee383989e40872c79dc803e97c03e7
vuonga1 <vuonga1@gene.com>
2022-03-14 16:29:04 -0700

    chore: move deprecated functions

--- gDRutils/R/deprecated.R
+++ gDRutils/R/deprecated.R
@@ -10,3 +10,17 @@ get_identifier <- function(k = NULL, simplify = TRUE) {
   .Deprecated("get_env_identifiers")
   get_env_identifiers(k = k, simplify = simplify)
 }
+
+
+#' @export
+set_identifier <- function(k, v) {
+  .Deprecated("set_env_identifier")
+  set_env_identifier(k = k, v = v)
+}
+
+
+#' @export
+reset_identifier <- function(k, v) {
+  .Deprecated("reset_env_identifiers")
+  reset_env_identifiers()
+}
