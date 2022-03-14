f853e4a8d86f59b3c9d779f74bb9c2b56f53fce3
vuonga1 <vuonga1@gene.com>
2022-03-14 12:59:42 -0700

    test: validate_polymapped_identifiers

--- gDRutils/tests/testthat/test-validate_identifiers.R
+++ gDRutils/tests/testthat/test-validate_identifiers.R
@@ -33,7 +33,7 @@ test_that(".check_required_identifiers works as expected", {
   single_map_ids <- ids
   single_map_ids[[1]] <- "Cinderella"
   obs <- gDRutils:::.check_required_identifiers(df, req_ids = req_ids, id_map = single_map_ids)
-  exp <- "specified value identifier(s): 'Cinderella' do not exist for standardized identifier(s): 'duration'"
+  exp <- "specified value identifier(s): 'Cinderella' do not exist for standardized identifier(s): 'duration'\n"
   expect_equal(obs, exp)
 
   # Missing identifier.
@@ -46,30 +46,68 @@ test_that(".check_required_identifiers works as expected", {
   poly_map_ids <- ids
   poly_map_ids[[1]] <- c("Cinderella", "Mulan")
   expect_error(gDRutils:::.check_required_identifiers(df, req_ids = req_ids, id_map = poly_map_ids),
-    regex = sprintf("more than one identifier value found for the required identifiers: '%s'", names(poly_map_ids)[1]))
+    regex = sprintf("more than one identifier value found for required identifiers: '%s'", names(poly_map_ids)[1]))
 })
 
 test_that(".check_polymapped_identifiers works as expected", {
   # Set up.
   nrow <- 5
-  req_ids <- get_required_identifiers()
-  nids <- length(req_ids)
+  exp_one_ids <- get_expect_one_identifiers()
+  nids <- length(exp_one_ids)
 
-  ids <- as.list(LETTERS[seq(nids)])
-  names(ids) <- req_ids
+  id_map <- as.list(LETTERS[seq(nids)])
+  names(id_map) <- exp_one_ids
 
   df <- as.data.frame(matrix(rep(0, nids * nrow), nrow = nrow, ncol = nids))
-  names(df) <- ids
+  names(df) <- id_map
 
-  # TODO
+  # All singletons.
+  obs <- gDRutils:::.check_polymapped_identifiers(df, exp_one_ids, id_map)
+  expect_equal(obs, NULL)
+
+  # Some polymappings.
+  some_poly_map <- id_map
+  some_poly_map[[1]] <- c(some_poly_map[[1]], "extra_item") 
+  obs <- gDRutils:::.check_polymapped_identifiers(df, exp_one_ids, id_map = some_poly_map)
+  expect_equal(obs, "more than one mapping for identifier(s): 'duration'\n")
+
+  # All polymappings.
+  all_poly_map <- id_map
+  all_poly_map <- lapply(seq_along(all_poly_map), function(x) c(all_poly_map[[x]], "extra_item"))
+  names(all_poly_map) <- names(id_map)
+  obs <- gDRutils:::.check_polymapped_identifiers(df, exp_one_ids, id_map = all_poly_map)
+  exp <- paste0(names(id_map), collapse = ", ")
+  expect_equal(obs, sprintf("more than one mapping for identifier(s): '%s'\n", exp))
 })
 
-test_that("gDRutils:::.singlefy_polymapped_identifiers works as expected", {
+test_that("gDRutils:::.validate_polymapped_identifiers works as expected", {
   # Set up.
+  nrow <- 5
+  exp_one_ids <- get_expect_one_identifiers()
+  nids <- length(exp_one_ids)
 
-  gDRutils:::.singlefy_polymapped_identifiers()
+  id_map <- as.list(LETTERS[seq(nids)])
+  names(id_map) <- exp_one_ids
 
-  gDRutils:::.singlefy_polymapped_identifiers()
+  df <- as.data.frame(matrix(rep(0, nids * nrow), nrow = nrow, ncol = nids))
+  names(df) <- id_map
+
+  # All singletons.
+  obs <- gDRutils:::.validate_polymapped_identifiers(df, exp_one_ids, id_map)
+  expect_equal(obs, id_map)
+
+  # Some polymappings.
+  some_poly_map <- id_map
+  some_poly_map[[1]] <- c(some_poly_map[[1]], "extra_item") 
+  obs <- gDRutils:::.validate_polymapped_identifiers(df, exp_one_ids, id_map = some_poly_map)
+  expect_equal(obs, id_map)
+
+  # All polymappings.
+  all_poly_map <- id_map
+  all_poly_map <- lapply(seq_along(all_poly_map), function(x) c(all_poly_map[[x]], "extra_item"))
+  names(all_poly_map) <- names(id_map)
+  obs <- gDRutils:::.validate_polymapped_identifiers(df, exp_one_ids, id_map = all_poly_map)
+  expect_equal(obs, id_map)
 })
 
 test_that("validate_identifiers works as expected", {
@@ -87,8 +125,7 @@ test_that("validate_identifiers works as expected", {
   # Single mapping but does not exist.
   single_map_ids <- ids
   single_map_ids[1] <- "Cinderella"
-  expect_error(validate_identifiers(df, identifiers = single_map_ids, req_ids = req_ids),
-    regex = "")
+  expect_error(validate_identifiers(df, identifiers = single_map_ids, req_ids = req_ids))
 
   # TODO
   # Polymapping not
