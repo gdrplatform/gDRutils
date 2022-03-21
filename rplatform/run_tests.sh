890650a3ee4877d4c26cd7fcf960d5219fde8d7d
vuonga1 <vuonga1@gene.com>
2022-03-21 06:03:14 -0700

    ci: add devtools::check

--- rplatform/run_tests.sh
+++ rplatform/run_tests.sh
@@ -15,9 +15,8 @@ Rscript -e "gDRstyle::lintPkgDirs('$repo_path')"
 echo ">>>>> RUNNING UNIT TESTS"
 Rscript -e "testthat::test_local(path = '$repo_path', stop_on_failure = TRUE)"
 
-#echo ">>>>> RUNNING DEVTOOLS::CHECK()"
-# TODO: Call gDRstyle::set_standard_check_env_vars()
-#Rscript -e "devtools::check('$repo_path', error_on = 'warning')
+echo ">>>>> RUNNING DEVTOOLS::CHECK()"
+Rscript -e "devtools::check('$repo_path', error_on = 'warning')
 
 echo ">>>>>>>> RUNNING CHECK DEPENDENCIES"
 Rscript -e "gDRstyle::checkDependencies(dep_path='/mnt/vol/rplatform/dependencies.yaml', desc_path='/mnt/vol/gDRutils/DESCRIPTION')"
