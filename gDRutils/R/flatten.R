bd9495e29646ecf00663eabc4f0697620ecfce04
vuonga1 <vuonga1@gene.com>
2022-03-21 06:01:53 -0700

    refactor: simplify checkmate assertions

--- gDRutils/R/flatten.R
+++ gDRutils/R/flatten.R
@@ -38,11 +38,7 @@ flatten <- function(tbl, groups, wide_cols, sep = "_") {
   checkmate::assert_character(groups)
   checkmate::assert_character(wide_cols)
   checkmate::assert_string(sep)
-  checkmate::assert_true(
-    methods::is(tbl, "data.table") ||
-      methods::is(tbl, "data.frame") ||
-        methods::is(tbl, "DFrame")
-  )
+  checkmate::assert_multi_class(tbl, c("data.frame", "DFrame"))
 
   if (!all(groups %in% colnames(tbl))) {
     stop(sprintf("missing expected uniquifying groups: '%s'",
