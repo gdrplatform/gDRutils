b48cb400bb65a8a92bd9819b79d8c428ac55ee6a
Bartosz Czech <bartosz.czech@contractors.roche.com>
2022-03-30 15:33:56 +0200

    tests: fix unit tests

--- gDRutils/tests/testthat/test-headers.R
+++ gDRutils/tests/testthat/test-headers.R
@@ -2,12 +2,13 @@ test_that("get_header works", {
   reset_env_identifiers()
 
   expect_error(get_header("BOGUS"))
-  expect_equal(get_header("manifest"), list(barcode = "Barcode", template = "Template", duration = "Duration"))
+  expect_equal(get_header("manifest"), list(barcode = c("Barcode", "Plate"), template = "Template", duration = "Duration"))
   expect_equal(length(get_header()), 12)
 
   set_env_identifier("duration", "TEST_DURATION")
-  expect_equal(get_header("manifest"), list(barcode = "Barcode", template = "Template", duration = "TEST_DURATION"))
+  expect_equal(get_header("manifest"), list(barcode = c("Barcode", "Plate"), template = "Template", duration = "TEST_DURATION"))
 
   reset_env_identifiers()
-  expect_equal(get_header("manifest"), list(barcode = "Barcode", template = "Template", duration = "Duration"))
+  expect_equal(get_header("manifest"), list(barcode = c("Barcode", "Plate"), template = "Template", duration = "Duration"))
 })
+
