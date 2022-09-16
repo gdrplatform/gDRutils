8d098770cc492d558e28f96e837b1823f07b262f
vuonga1 <allison.vuong@gmail.com>
2022-09-16 16:57:03 -0700

    feat: promote_fields and demote_fields in an SE

--- gDRutils/R/concatentate_SEs.R
+++ gDRutils/R/concatentate_SEs.R
@@ -3,6 +3,20 @@ has_nested_field <- function(asy, nested_field) {
   all(nested_field %in% colnames(df))
 }
 
+.validate_final_assays <- function(final_assays) {
+  if (length(final_assays) == 0L) {
+    stop(sprintf("unable to find nested fields: '%s', perhaps you intended to call 'demote_fields'?",
+      paste0(fields, collapse = ", "))) 
+  } else if (length(final_assays) > 1L) {
+    exp <- final_assays[[1]]
+    for (i in 2:length(final_assays)) {
+      curr_assay <- final_assays[[i]]
+      stopifnot(rownames(exp$rowData) == rownames(curr_assay$rowData))
+      stopifnot(rownames(exp$colData) == rownames(curr_assay$colData))
+    }
+  }
+}
+
 #' Demote a metadata field to be represented as a nested field of the \code{BumpyMatrix} assay.
 #'
 #' @param se \code{SummarizedExperiment} object.
@@ -16,10 +30,38 @@ demote_fields <- function(se, fields) {
   rowmd <- colnames(rowData(se)) 
   colmd <- colnames(colData(se))
 
-  if (any(nested_fields <- !fields %in% c(rowmd, colmd))) {
-    stop(sprintf("fields '%s' are already demoted fields, perhapy you intended to call 'promote_fields'?",
-      paste0(fields[nested_fields], collapse = ", ")))
+  if (any(are_nested_fields <- !fields %in% c(rowmd, colmd))) {
+    stop(sprintf("field(s) '%s' are already demoted fields, perhapy you intended to call 'promote_fields'?",
+      paste0(fields[are_nested_fields], collapse = ", ")))
   }
+
+  rowmd <- setdiff(rowmd, fields)
+  colmd <- setdiff(colmd, fields)
+
+  final_assays <- list()
+  asynames <- SummarizedExperiment::assayNames(se)
+  for (asyname in asynames) {
+    asy <- SummarizedExperiment::assay(se, asyname)
+    df <- S4Vectors::DataFrame(as.data.frame(convert_se_assay_to_dt(se, asyname, include_metadata = TRUE)))
+    final_assays[[asyname]] <- .transform_df_to_matrix(df = df,
+      row_fields = rowmd,
+      column_fields = colmd,
+      nested_fields = setdiff(colnames(df), c(rowmd, colmd))) 
+  }
+
+  .validate_final_assays(final_assays)
+
+  assay_list <- lapply(final_assays, function(x) {
+    x$mat
+  })
+
+  final_SE <- SummarizedExperiment::SummarizedExperiment(
+    assays = assay_list,
+    rowData = final_assays[[1]]$rowData,
+    colData = final_assays[[1]]$colData,
+    metadata = S4Vectors::metadata(se)
+  )
+  final_SE
 }
 
 #' Promote a nested field to be represented as a metadata field of the \code{SummarizedExperiment}
@@ -34,7 +76,9 @@ demote_fields <- function(se, fields) {
 #' @seealso demote_fields
 #' @export
 promote_fields <- function(se, fields, MARGIN = c(1, 2)) {
-  MARGIN <- match.arg(MARGIN)
+  if (length(MARGIN) != 1L || !MARGIN %in% c(1, 2)) {
+    stop("invalid 'MARGIN' argument, must be either '1' or '2'")
+  }
 
   rowmd <- colnames(rowData(se)) 
   colmd <- colnames(colData(se))
@@ -48,6 +92,8 @@ promote_fields <- function(se, fields, MARGIN = c(1, 2)) {
     rowmd <- c(rowmd, fields)
   } else if (MARGIN == 2) {
     colmd <- c(colmd, fields)
+  } else {
+    stop("invalid input for argument 'MARGIN'")
   }
 
   final_assays <- list()
@@ -65,21 +111,21 @@ promote_fields <- function(se, fields, MARGIN = c(1, 2)) {
     }
   }
 
-  if (length(final_SE) == 0L) {
-    stop(sprintf("unable to find nested fields: '%s', perhaps you intended to call 'demote_fields'?",
-      paste0(fields, collapse = ", "))) 
-  }
+  .validate_final_assays(final_assays)
 
-  # Ensure order.
-  final_assays <- lapply(final_assays, function(x) {x[rownames(rowData), colnames(colData)]})
+  assay_list <- lapply(final_assays, function(x) {
+    x$mat
+  })
+
   final_SE <- SummarizedExperiment::SummarizedExperiment(
-    assays = final_assays,
-    rowData = ,
-    colData = ,
+    assays = assay_list,
+    rowData = final_assays[[1]]$rowData,
+    colData = final_assays[[1]]$colData,
     metadata = S4Vectors::metadata(se)
   )
   final_SE
 }
+
 
 #' Transform a \code{DFrame} to a \code{BumpyMatrix}.
 #'
@@ -94,9 +140,13 @@ promote_fields <- function(se, fields, MARGIN = c(1, 2)) {
 #' @param nested_fields Character vector representing the columns that should become nested in the
 #' final matrix object.
 #'
-#' @return A \code{BumpyMatrix} object.
+#' @return A named list of \code{mat}, \code{rowData}, and \code{colData}
+#' where \code{mat} is a \code{BumpyMatrix} object, ordered by the rownames of
+#' \code{rowData} and \code{colData}.
+#'
 #' @seealso promote_fields demote_fields 
 #'
+#' @details 
 #' @noRd
 #' @importFrom BumpyMatrix splitAsBumpyMatrix
 #' @keywords internal
@@ -106,10 +156,24 @@ promote_fields <- function(se, fields, MARGIN = c(1, 2)) {
     stop(sprintf("found columns in 'df' not specified as row, column, or nested fields",
       paste0(missing, collapse = ", ")))
   }
+  if (any(is_duplicated <- duplicated(c(row_fields, column_fields, nested_fields)))) {
+    stop(sprintf("fields: '%s' are duplicated across arguments 'row_fields', 'column_fields', 'nested_fields'",
+      c(row_fields, column_fields, nested_fields)[is_duplicated]))
+  }
+
 
   df$row <- S4Vectors::selfmatch(df[, row_fields])
+  rowData <- unique(df[, c("row", row_fields)])
+  rownames(rowData) <- rowData$row # Set rownames to maintain association btw rowData and mat.
+
   df$column <- S4Vectors::selfmatch(df[, column_fields])
-  BumpyMatrix::splitAsBumpyMatrix(df[, nested_fields], row = df$row, column = df$column)
+  colData <- unique(df[, c("column", column_fields)])
+  rownames(colData) <- colData$column # Set rownames to maintain association btw colData and mat.
+
+  mat <- BumpyMatrix::splitAsBumpyMatrix(df[, nested_fields], row = df$row, column = df$column)
+  list(mat = mat[rownames(rowData), rownames(colData)],
+       rowData = rowData[, setdiff(colnames(rowData), "row")],
+       colData = colData[, setdiff(colnames(colData), "column")])
 }
 
 #' Aggregate a \code{BumpyMatrix} assay by a given aggreation function.
@@ -132,14 +196,23 @@ aggregate_assay <- function(asy, by, FUN) {
   row.field <- "row"
   column.field <- "column"
 
-  df <- BumpyMatrix::unsplitAsDataFrame(asy, row.names = TRUE, column.names = TRUE, row.field = row.field, column.field = column.field)
+  df <- BumpyMatrix::unsplitAsDataFrame(asy,
+    row.names = TRUE,
+    column.names = TRUE,
+    row.field = row.field,
+    column.field = column.field)
   if (!all(present <- by %in% setdiff(colnames(df), c(row.field, column.field)))) {
     stop(sprintf("specified 'by' columns: '%s' are not present in 'asy'", paste0(by[!present], collapse = ", ")))
   }
 
   by <- c(row.field, column.field, by)
-  agg <- stats::aggregate(x = data.frame(df[, setdiff(colnames(df), by)]), by = data.frame(df[, by]), FUN = FUN, na.rm = TRUE)
-  mat <- BumpyMatrix::splitAsBumpyMatrix(agg[, setdiff(colnames(agg), c(row.field, column.field))], row = agg[[row.field]], column = agg[[column.field]])
+  agg <- stats::aggregate(x = data.frame(df[, setdiff(colnames(df), by)]),
+    by = data.frame(df[, by]),
+    FUN = FUN,
+    na.rm = TRUE)
+  mat <- BumpyMatrix::splitAsBumpyMatrix(agg[, setdiff(colnames(agg), c(row.field, column.field))],
+    row = agg[[row.field]],
+    column = agg[[column.field]])
 
   mat[rownames(asy), colnames(asy)] # Ensure order is the same.
 }
