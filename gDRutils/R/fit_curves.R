851e8b019a64b7321ccc747d208434cd9d24bc12
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2023-02-17 10:03:11 +0100

    chore: make linter happy

--- gDRutils/R/fit_curves.R
+++ gDRutils/R/fit_curves.R
@@ -237,7 +237,14 @@ logisticFit <-
     out <- tryCatch({
       non_na_avg_norm <- !is.na(df_$norm_values)
       if (sum(non_na_avg_norm) < n_point_cutoff) {
-        stop(fitting_handler("too_few_fit", message = sprintf("not enough data points (%i < %i) to perform fitting", sum(non_na_avg_norm), n_point_cutoff)))
+        stop(fitting_handler(
+          "too_few_fit",
+          message = sprintf(
+            "not enough data points (%i < %i) to perform fitting",
+            sum(non_na_avg_norm),
+            n_point_cutoff
+          )
+        ))
       }
       if (length(unique(df_$norm_values[non_na_avg_norm])) == 1L) {
         stop(fitting_handler("constant_fit", message = "only 1 normalized value detected, setting constant fit"))
@@ -294,7 +301,13 @@ logisticFit <-
 
       f_pval <- .calculate_f_pval(df1, df2, RSS1, RSS2)
       if ((!force_fit) & ((exists("f_pval") & !is.na(f_pval) & f_pval >= pcutoff) | is.na(out$ec50))) {
-        stop(fitting_handler("constant_fit", message = sprintf("fit is not statistically significant (p=%.2f), setting constant fit", f_pval)))
+        stop(fitting_handler(
+          "constant_fit",
+          message = sprintf(
+            "fit is not statistically significant (p=%.2f), setting constant fit",
+            f_pval
+          )
+        ))
       }
 
       # Add xc50 = +/-Inf for any curves that do not reach RV/GR = 0.5.
