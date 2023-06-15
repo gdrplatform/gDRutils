945c88ea29b85db81cf9a75bf5c8bfc8679ce48f
czechb3 <bartosz.w.czech@gmail.com>
2023-06-15 16:03:52 +0200

    refactor: update as per review

--- R/packages.R
+++ R/packages.R
@@ -4,4 +4,18 @@
 #' @importFrom methods is
 
 .datatable.aware <- TRUE
+
+
+
+# Prevent R CMD check from complaining about the use of pipe expressions
+# standard data.table variables
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(
+    c(
+      "rId",
+      "cId"
+    ), 
+    utils::packageName())
+}
+
 NULL
