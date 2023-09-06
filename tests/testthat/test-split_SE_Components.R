35378d937af0e61e3ec698b19b93c382c2e35c2a
czechb3 <bartosz.w.czech@gmail.com>
2023-09-06 09:11:29 +0200

    test: fix unit tests

--- tests/testthat/test-split_SE_Components.R
+++ tests/testthat/test-split_SE_Components.R
@@ -47,15 +47,6 @@ test_that("split_SE_components splits the correct columns", {
   expect_equal(names(md5$treatment_md), c(new_identifier_name, "DrugName", "drug_moa", "Duration"))
 })
 
-
-test_that("split_SE_components throws a warning for bad cell line metadata", {
-  df2 <- test_df
-  df2$CellLineName[7] <- "Abnormality"
-  expect_warning(split_SE_components(df2), 
-    regexp = "'CellLineName' not metadata for unique cell line identifier column")
-})
-
-
 test_that("add_rownames_to_metadata works as expected", {
   cols <- c("a", "b")
   md <- data.frame(a = LETTERS, b = letters, c = paste0(LETTERS, letters))
