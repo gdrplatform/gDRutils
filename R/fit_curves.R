2f3b782f87459e92eb49659eb11069bb4fa65957
Marc Hafner <hafnerm6@gene.com>
2023-09-21 10:43:37 -0700

    Debug case of conc=0 for evaluating efficacy with flat fit

--- R/fit_curves.R
+++ R/fit_curves.R
@@ -414,7 +414,11 @@ predict_efficacy_from_conc <- function(c, x_inf, x_0, ec50, h) {
   checkmate::assert_numeric(h)
   assert_equal_input_len(outlier = c, x_inf, x_0, ec50, h)
 
-  x_inf + (x_0 - x_inf) / (1 + (c / ec50) ^ h)
+  if (c > 0) {
+    x_inf + (x_0 - x_inf) / (1 + (c / ec50) ^ h)
+  } else { # avoid issues with c=0 for DRCConstantFitResult
+    x_0
+  }
 }
 
 
