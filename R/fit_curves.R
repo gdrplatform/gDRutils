851402d97873efa26463e935ad6d40fd7eb739ab
Janina Smola <janina.smola@contractors.roche.com>
2023-09-22 15:16:37 +0200

    chore: vectorize predict_efficacy_from_conc

--- R/fit_curves.R
+++ R/fit_curves.R
@@ -414,13 +414,9 @@ predict_efficacy_from_conc <- function(c, x_inf, x_0, ec50, h) {
   checkmate::assert_numeric(h)
   assert_equal_input_len(outlier = c, x_inf, x_0, ec50, h)
 
-  unlist(lapply(c, function(c_) {
-    if (c_ > 0) {
-      x_inf + (x_0 - x_inf) / (1 + (c_ / ec50) ^ h)
-    } else { # avoid issues with c=0 for DRCConstantFitResult
-      x_0
-    }
-  }))
+  ifelse(c > 0, 
+         x_inf + (x_0 - x_inf) / (1 + (c / ec50) ^ h), 
+         x_0) # avoid issues with c=0 for DRCConstantFitResult
 }
 
 
