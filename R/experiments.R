ca53bb852f8e0a7cf360c09c9e08ac0046695d48
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2024-05-17 20:07:09 +0200

    refactor: simplify logic of supported experiments

--- R/experiments.R
+++ R/experiments.R
@@ -1,31 +1,18 @@
 ## The following are the default values that will be used for handling MAE experiments
-EXPERIMENT_GROUPS <-
-  list(`single-agent` = c(`single-agent` = "single-agent",
-                          `co-dilution` = "co-dilution"),
-       combination = "combination")
+SUPPORTED_EXPERIMENTS <- c("single-agent", "combination")
 
 #' get_experiment_groups
 #'
-#' get experiment groups
-#' @param type String indicating the name of an assay group.
-#' Defaults to all experiment groups.
+#' get supported experiments
 #' @keywords experiment
 #'
-#' @return list with experiment groups or string (if type not NULL)
+#' @return charvec with supported experiment names
 #'
-#' @examples 
-#' get_experiment_groups()
-#' 
+#' @examples
+#' get_supported_experiments()
+#'
 #' @export
 #' @author Arkadiusz Gladki <arkadiusz.gladki@@contractors.roche.com>
-get_experiment_groups <- function(type = NULL) {
-  
-  checkmate::assert_choice(type, names(EXPERIMENT_GROUPS), null.ok = TRUE)
-  
-  if (!is.null(type)) {
-    EXPERIMENT_GROUPS[[type]]
-  } else {
-    EXPERIMENT_GROUPS
-  }
-  
+get_supported_experiments <- function() {
+  SUPPORTED_EXPERIMENTS
 }
