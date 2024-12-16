c30f69dd3fe3d94a810a0ea365dc18e26bc52949
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2024-12-16 17:23:17 +0100

    chore: minor improvement

--- R/split_SE_components.R
+++ R/split_SE_components.R
@@ -61,7 +61,6 @@ split_SE_components <- function(df_, nested_keys = NULL, combine_on = 1L) {
     get_header("metrics_results"), get_env_identifiers("concentration", simplify = TRUE),
     identifiers_md$well_position, identifiers_md$template, nested_keys,
     get_header("scores"), get_header("excess"), get_header("isobolograms")))
-  data_fields <- unique(data_fields)
   data_cols <- data_fields[data_fields %in% all_cols]
   md_cols <- setdiff(all_cols, data_cols) 
   md <- unique(df_[, md_cols]) 
